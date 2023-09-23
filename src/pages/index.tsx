a6c3c60bbe99b2d4c65d96f68ee9d94cda2f8f36
James Pike <jamesandrewpike@gmail.com>
2023-09-23 16:46:31 -0400

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const DashboardPage: FC = function () {
                     <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                 </a>
                 <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl xl:text-6xl dark:text-white">We invest in the world’s potential</h1>
-                <p className="max-w-xl mx-auto mb-6 font-light text-gray-500 lg:mx-0 xl:mb-8 md:text-lg xl:text-xl dark:text-gray-400">At Cathy🔬 we focus on markets where innovation can unlock long-term value and drive economic growth.</p>
+                <p className="max-w-xl mx-auto mb-6 font-light text-gray-500 lg:mx-0 xl:mb-8 md:text-lg xl:text-xl dark:text-gray-400">At webdev🔬 we focus on markets where innovation can unlock long-term value and drive economic growth.</p>
                 <form className="max-w-lg mx-auto lg:ml-0" action="#">
                     <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
                     <div className="relative">
